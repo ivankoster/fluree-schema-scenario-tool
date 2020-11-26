3861d7eae471c4f46fced9dc13132a22ab0c5540
Rob J Meijer <rob@dnpa.nl>
2020-11-26 13:53:05 +0100

    added auth experiment

--- demo.sh
+++ demo.sh
@@ -3,3 +3,6 @@
 ./fsst --docker --dir experiments --target user_ok --tag fluree-0.15.7
 ./fsst --docker --dir experiments --target user_not_ok --tag fluree-0.15.7
 ./fsst --docker --dir experiments --target user_both --tag fluree-0.15.7
+./fsst --docker --dir experiments --target auth_ok --tag fluree-0.15.7
+./fsst --docker --dir experiments --target auth_not_ok --tag fluree-0.15.7
+./fsst --docker --dir experiments --target auth_both --tag fluree-0.15.7
